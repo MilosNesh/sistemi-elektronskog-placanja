76fbd3dd3aa6bf2fc011b92d4d3ca04e524e2052
MilosNesh <neskovic.milos02@gmail.com>
2025-12-27 22:41:37 +0100

    initialized projects

---
+++ PSPBackend/src/main/java/org/example/pspbackend/PspBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.pspbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PspBackendApplication.class, args);
+    }
+
+}
---
+++ PSPBackend/src/test/java/org/example/pspbackend/PspBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pspbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
