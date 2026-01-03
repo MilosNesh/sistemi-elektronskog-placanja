e0fb3541e4dd705957405008aa8f50a2eb959c1f
MilosNesh <neskovic.milos02@gmail.com>
2026-01-03 15:44:14 +0100

    feat: implemented registration on frontend

--- WebShopFrontend/src/app/auth/auth.service.ts
+++ WebShopFrontend/src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginDetails } from '../models/login-details.model';
 import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +15,7 @@ export class AuthService {
     return this.http.post("http://localhost:8080/user/login", loginDetails, {responseType: 'text'});
   }
   
+  public register(user: User) : Observable<User> {
+    return this.http.post<User>("http://localhost:8080/user/register", user);
+  }
 }
